2a450cc9d2a660584de66a43749727c2cd57e57c
Moussa lidya <moussalidya07@gmail.com>
2026-01-21 16:37:17 +0300

    Monpremierserveurnode.js

---
+++ monscript.js
@@ -0,0 +1,16 @@
+// Définition de la fonction hello
+// fonction est un mot-clé en JavaScript pour définir une fonction
+
+
+
+
+function hello(){
+
+    // Affichage du message dans le terminal 
+    // on utilisant console log qui est une méthode d'affichage dans la console suivie de la chaîne de caractères à afficher (string)
+    console.log("Hello, world!"); 
+    console.log ("je suis dans node.js");
+}    
+
+// Appel de la fonction hello dans le terminal
+hello();
