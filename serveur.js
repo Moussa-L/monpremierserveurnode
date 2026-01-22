6d76600c19a5c8b4afd4ca24c59855b1f392ec8d
Moussa lidya <moussalidya07@gmail.com>
2026-01-22 16:34:15 +0300

    j'ai relier les serveurs et l'applications est l'express . l'app.js est pour gérer les routes

--- serveur.js
+++ serveur.js
@@ -2,14 +2,30 @@
 
 // Importation du module HTTP est de stockée dans une constante (variable)nommée http
 const http = require('http');
+const app = require('./app');
 // Maintenant, je crée un serveur web en utilisant la méthode createServer du module HTTP
+
+app.set('port',3000);
+
+//app.use((req, res) => {
+   // res.end('Bonjour je suis le serveur Node.js!');
+ //}); 
+
+
 
 
 // ici j'ai affaire a une fonction fléchée (function) qui prend deux paramètres req (requête) et res (réponse)
-const server = http.createServer((req, res) => {
-    //
-    res.end('Bonjour je suis le serveur Node.js!');
- }); 
+//const server = http.createServer((req, res) => {
+
+
+
+const server = http.createServer(app);
+
+const date = new Date();
+
 // Le serveur écoute sur le port 
 // Port signifie l'adresse locale de mon ordinateur
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Le serveur est démarré sur le port 3000');
+    console.log( date.toLocaleString());
+});
