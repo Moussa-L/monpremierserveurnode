cdcd927da05960cadd1008cff2f210895f7c8c0b
Moussa lidya <moussalidya07@gmail.com>
2026-01-22 16:32:05 +0300

    Créer l'applications expressjs

---
+++ app.js
@@ -0,0 +1,14 @@
+// Ici , je crée mon application expressJS
+
+// Importation du framework et de le stockée dans une constante (variable) nommée expressjs
+const express = require('express');
+
+//j'initialise mon application expressJS
+const app = express();
+
+
+
+//J'exorte mon application comme un module
+module.exports = app;
+
+// Je définis une route pour la racine de mon application
