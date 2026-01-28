dd61d493163087c863e210d2e9aa9aca87395d7e
Moussa lidya <moussalidya07@gmail.com>
2026-01-28 14:12:32 +0300

    APP.JS

--- app.js
+++ app.js
@@ -39,7 +39,13 @@ app.use((req, res,next) => {
 });
     
 //mon fruit
-app.use('/api/fruit', (req, res) => {
+// j'utilise la méthode GET pour récupérer les fruits
+/* app.get('/api/fruit', (req, res,) => {
+    app.get: est une méthode ExpressJS utilisée pour gérer les requêtes HTTP GET.
+    /api/fruit: est le chemin de la route où les fruits seront récupérés.
+    const fruit = [ ... ]: est un tableau contenant des objets représentant des fruits, chacun avec des propriétés telles que id, nom, descriptions et prix. 
+*/
+app.get('/api/fruit', (req, res,) => {
 const fruit = [
         {
             //ici je crée un objet fruit
@@ -63,6 +69,7 @@ const fruit = [
             descriptions:"L'orange est un agrume sucré et acidulé, riche en vitamine C .",
             prix:12 
         }
+        
 ];
 
 
@@ -75,7 +82,40 @@ res.status(200).json(fruit);
 }); 
 
 
+//je vais créér une route expresseJS en POST pour ajouter un fruit
 
+/*app.post('/api/fruit', (req, res) => {
+    app.post: est une méthode ExpressJS utilisée pour créer une nouvelle ressource.
+    /api/fruit: est le chemin de la route où le fruit sera ajouté.
+    (req, res) => { ... }: est une fonction de rappel (callback) qui sera exécutée lorsque cette route sera appelée. Elle prend deux paramètres :
+    req: représente la requête entrante.
+    res: représente la réponse que le serveur enverra au client.
+});*/
+app.post('/api/fruit', (req, res) => {
+    res.status(201).json({
+        message: "Fruit ajouté avec succès"
+    });
+});
+
+//je vais créér une route expresseJS en PUT pour modifier un fruit
+
+/*app.put('/api/fruit', (req, res) => {
+    app.put: est une méthode ExpressJS utilisée pour modifier une ressource existante.
+    /api/fruit: est le chemin de la route où la modification sera effectuée.
+    (req, res) => { ... }: est une fonction de rappel (callback) qui sera exécutée lorsque cette route sera appelée. Elle prend deux paramètres :
+    req: représente la requête entrante.
+    res: représente la réponse que le serveur enverra au client.
+});*/
+/*res.status(200).json({
+        message: "Fruit modifié avec succès"
+    });
+    res.status(200).json: est une méthode utilisée pour envoyer une réponse HTTP avec un code de statut 200 (OK) et un objet JSON contenant un message indiquant que le fruit a été modifié avec succès.
+    */ 
+app.put('/api/fruit', (req, res) => {
+    res.status(200).json({
+        message: "Fruit modifié avec succès"
+    });
+});
 // j'exporte mon application expressJS pour pouvoir l'utiliser dans serveur.js
 
 module.exports = app;
